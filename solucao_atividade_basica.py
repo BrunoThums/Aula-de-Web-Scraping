0e3aa00c9c3fcf9ca7ceae6826762d8ba7583291
Bruno Brill Thums <bruno.thums@universo.univates.br>
2023-11-08 14:47:27 -0300

    Update solucao_atividade_basica.py

--- solucao_atividade_basica.py
+++ solucao_atividade_basica.py
@@ -1,25 +1,21 @@
 import scrapy
 
-#opção 1: pegar todas as frases e fazer as verificações (autor e tag)
-#opção 2: já verificar se o autor bate, e se sim, verificar a tag
-
 class BuscarcSpider(scrapy.Spider):
     name = "buscarC"
-    allowed_domains = ["quotesto.scrape.com"]
     start_urls = ["http://quotes.toscrape.com/tag/life/page/1/","http://quotes.toscrape.com/tag/life/page/2/"]
     global autor, frase
     global tags
     def parse(self, response):
         frases = response.xpath('//div//div[contains(@class,"row")][2]/div[1]/div')
-        print("###################",frases,"###################")
+        #print("###################",frases,"###################")
         for frase in frases:
             print("Frase:",frase)
             autor_t = frase.xpath('.//small[contains(@class,"author")]/text()').get()
             tag_t = frase.xpath('.//div[contains(@class,"tags")]//a/text()').getall()
-            print("##################\n",autor_t)
-            print("##################\n",tag_t)
+            #print("##################\n",autor_t)
+            #print("##################\n",tag_t)
             if(autor_t=="Mark Twain" and "life" in tag_t):
-                print("\nok\n")
+                #print("\nok\n")
                 frase_t = frase.xpath('.//span[contains(@class,"text")]/text()').get()
                 item = {
                     'autor': autor_t,
