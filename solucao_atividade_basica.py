6cbd499cfbc1f2fe2095291ab36ad9c3da3547f9
Bruno Brill Thums <bruno.thums@universo.univates.br>
2023-11-08 15:07:12 -0300

    Update solucao_atividade_basica.py

--- solucao_atividade_basica.py
+++ solucao_atividade_basica.py
@@ -3,20 +3,18 @@ import scrapy
 class BuscarcSpider(scrapy.Spider):
     name = "buscarC"
     start_urls = ["http://quotes.toscrape.com/tag/life/page/1/","http://quotes.toscrape.com/tag/life/page/2/"]
-    global autor, frase
-    global tags
     def parse(self, response):
-        frases = response.xpath('//div//div[contains(@class,"row")][2]/div[1]/div')
+        conteinerFrases = response.xpath('//div//div[contains(@class,"row")][2]/div[1]/div')
         #print("###################",frases,"###################")
-        for frase in frases:
-            print("Frase:",frase)
-            autor_t = frase.xpath('.//small[contains(@class,"author")]/text()').get()
-            tag_t = frase.xpath('.//div[contains(@class,"tags")]//a/text()').getall()
+        for conteinerFrase in conteinerFrases:
+            #print("Frase:",conteinerFrase)
+            autor_t = conteinerFrase.xpath('.//small[contains(@class,"author")]/text()').get()
+            tag_t = conteinerFrase.xpath('.//div[contains(@class,"tags")]//a/text()').getall()
             #print("##################\n",autor_t)
             #print("##################\n",tag_t)
             if(autor_t=="Mark Twain" and "life" in tag_t):
                 #print("\nok\n")
-                frase_t = frase.xpath('.//span[contains(@class,"text")]/text()').get()
+                frase_t = conteinerFrase.xpath('.//span[contains(@class,"text")]/text()').get()
                 item = {
                     'autor': autor_t,
                     'tags': tag_t,
